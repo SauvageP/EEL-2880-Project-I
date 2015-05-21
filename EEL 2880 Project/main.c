2eb3c5c6482152cacfbf54a4dbc2bce5e246869f
Perry R. Gabriel <user@me.com>
2015-05-21 12:14:31 -0400

    Initial Commit

---
+++ EEL 2880 Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  EEL 2880 Project
+//
+//  Created by Perry R Gabriel on 5/21/15.
+//  Copyright (c) 2015 Perry R Gabriel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
